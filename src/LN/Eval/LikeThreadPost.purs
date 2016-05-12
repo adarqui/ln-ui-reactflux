d35f0e7729b795c0e41f526de57cd8f909199bce
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-12 13:19:54 -0400

    need to turn some arrays in state, into maps

--- src/LN/Eval/LikeThreadPost.purs
+++ src/LN/Eval/LikeThreadPost.purs
@@ -4,7 +4,6 @@ module LN.Eval.LikeThreadPost (
 
 
 
-import Control.Monad.Aff.Console   (log)
 import Data.Maybe                  (Maybe(..), maybe)
 import Data.Either                 (Either(..))
 import Data.Functor                (($>))
@@ -12,6 +11,7 @@ import Halogen                     (get, modify, liftAff')
 import Optic.Core                  ((^.), (..))
 import Prelude                     (bind, pure, show, ($), (<>))
 
+import LN.Api                      (rd, postThreadPostLike_ByThreadPostId')
 import LN.Component.Types          (EvalEff)
 import LN.Input.LikeThreadPost     (InputLikeThreadPost(..))
 import LN.Input.Types              (Input(..))
@@ -25,16 +25,23 @@ eval_LikeThreadPost :: EvalEff
 
 
 eval_LikeThreadPost eval (CompLikeThreadPost (InputLikeThreadPost_Like thread_post_id) next) = do
+  let tplr = mkThreadPostLikeRequest Like Nothing
+  etplr <- rd $ postThreadPostLike_ByThreadPostId' thread_post_id tplr
+  case etplr of
+    Left err    -> pure next
+    Right rtplr -> pure next
   pure next
 
 
 
 eval_LikeThreadPost eval (CompLikeThreadPost (InputLikeThreadPost_Neutral thread_post_id) next) = do
+  let tplr = mkThreadPostLikeRequest Neutral Nothing
   pure next
 
 
 
 eval_LikeThreadPost eval (CompLikeThreadPost (InputLikeThreadPost_Dislike thread_post_id) next) = do
+  let tplr = mkThreadPostLikeRequest DontLike Nothing
   pure next
 
 
