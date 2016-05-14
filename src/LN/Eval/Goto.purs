aa7786335c3beb77b37897126796df86a8c7ae2c
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-14 17:06:45 -0400

    getting resources to display

--- src/LN/Eval/Goto.purs
+++ src/LN/Eval/Goto.purs
@@ -61,6 +61,27 @@ eval_Goto eval (Goto route next) = do
         moffset
       eval (GetUsers next) $> unit
 
+    (PortalResources params) -> do
+      let moffset = elemBy (\(Tuple k v) -> k == "offset") params
+      maybe
+        (pure unit)
+        (\(Tuple k offset) -> do
+          pageInfo <- gets _.resourcesPageInfo
+          modify (_{ resourcesPageInfo = pageInfo { currentPage = maybe 1 id (fromString offset) } })
+          pure unit)
+        moffset
+      eval (GetResources next) $> unit
+
+    (PortalLeurons params) -> do
+      let moffset = elemBy (\(Tuple k v) -> k == "offset") params
+      maybe
+        (pure unit)
+        (\(Tuple k offset) -> do
+          pageInfo <- gets _.leuronsPageInfo
+          modify (_{ leuronsPageInfo = pageInfo { currentPage = maybe 1 id (fromString offset) } })
+          pure unit)
+        moffset
+      eval (GetLeurons next) $> unit
 
 
     (Organizations (Show org_name)) -> eval (GetOrganization org_name next) $> unit
@@ -147,6 +168,9 @@ eval_Goto eval (Goto route next) = do
 
       eval (GetOrganizationForumBoardThread org_name forum_name board_name thread_name next) $> unit
 
+
+    (Resources params) -> eval (GetResources next) $> unit
+    (Leurons params) -> eval (GetLeurons next) $> unit
 
 
     (Users (Show user_nick)) -> do
