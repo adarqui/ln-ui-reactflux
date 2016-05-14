fb7db7ea829731c79642126033efab3a587cb402
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-14 15:11:55 -0400

    getting resources

--- src/LN/Eval/Resources.purs
+++ src/LN/Eval/Resources.purs
@@ -6,7 +6,7 @@ module LN.Eval.Resources (
 
 import Halogen                       (get, gets, modify)
 import Daimyo.Data.ArrayList         (listToArray, arrayToList)
-import Data.Array                    (nub, filter, head)
+import Data.Array                    (nub, filter, head, zip)
 import Data.Either                   (Either(..))
 import Data.Map                      as M
 import Data.Maybe                    (Maybe(..), maybe)
@@ -14,7 +14,7 @@ import Data.Tuple                    (Tuple(..))
 import Optic.Core                    ((^.), (..))
 import Prelude                       (bind, pure, not, map, ($), (+), (*), (-), (/))
 
-import LN.Api                        (rd, getResourcesCount', getResourcePacks')
+import LN.Api                        (rd, getResourcesCount', getResourcePacks', getResourcePacks)
 import LN.Api.Internal.String        as ApiS
 import LN.Component.Types            (EvalEff)
 import LN.Input.Types                (Input(..))
@@ -25,9 +25,31 @@ import LN.T
 eval_GetResources :: EvalEff
 eval_GetResources eval (GetResources next) = do
 
-  eresource_packs <- rd $ getResourcePacks'
-  case eresource_packs of
-       Left err -> pure next
-       Right (ResourcePackResponses resource_packs) -> do
-         modify (_{ resources = resource_packs.resourcePackResponses })
-         pure next
+  pageInfo <- gets _.resourcesPageInfo
+
+  ecount <- rd $ getResourcesCount'
+  case ecount of
+    Left err -> pure next
+    Right (CountResponses counts) -> do
+
+      let count = maybe 0 (\(CountResponse count) -> count.n) (head counts.countResponses)
+
+      modify (_ { resourcesPageInfo = pageInfo { totalResults = count, totalPages = (count / pageInfo.resultsPerPage)+1 } })
+
+      eresource_packs <- rd $ getResourcePacks [Limit pageInfo.resultsPerPage, Offset ((pageInfo.currentPage-1) * pageInfo.resultsPerPage), SortOrder pageInfo.sortOrder, Order pageInfo.order]
+      case eresource_packs of
+           Left err -> pure next
+           Right (ResourcePackResponses resource_packs) -> do
+
+             let
+              users = map (\(ResourcePackResponse pack) -> pack.user) resource_packs.resourcePackResponses
+              resources_map =
+                M.fromFoldable
+                  $ zip (map (\(ResourcePackResponse p) -> p.resource ^. _ResourceResponse .. id_) resource_packs.resourcePackResponses) resource_packs.resourcePackResponses
+
+
+             eval (GetUsers_MergeMap_ByUser users next)
+
+
+             modify (_{ resources = resources_map })
+             pure next
