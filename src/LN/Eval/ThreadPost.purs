a117e25bf00e4a4ac50ba0b7fc897fd4193a7947
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-16 20:24:43 -0400

    cleanup

--- src/LN/Eval/ThreadPost.purs
+++ src/LN/Eval/ThreadPost.purs
@@ -8,18 +8,17 @@ import Data.Either          (Either(..))
 import Data.Functor         (($>))
 import Data.Map             as M
 import Data.Maybe           (Maybe(..), maybe)
-import Data.Maybe.Unsafe    (fromJust)
 import Halogen              (get, modify)
 import Optic.Core           ((^.), (..))
 import Prelude              (bind, pure, ($))
 
-import LN.Api               (rd, postThreadPost_ByThreadId')
+import LN.Api               (rd, postThreadPost_ByThreadId', getThreadPostPack')
 import LN.Component.Types   (EvalEff)
 import LN.Input.ThreadPost  (InputThreadPost(..))
 import LN.Input.Types       (Input(..))
-import LN.T
-import LN.T.Pack.ThreadPost.Response (defaultThreadPostPackResponse)
-import LN.T.User.Helpers    (userResponseToSanitizedResponse)
+import LN.T                 (_ThreadPackResponse, ThreadPostRequest (..)
+                            , ThreadPostResponse (..), _ThreadResponse, thread_, id_
+                            , PostData (..))
 
 
 
@@ -36,18 +35,22 @@ eval_ThreadPost eval (CompThreadPost InputThreadPost_Post next) = do
   st <- get
   let thread_id = maybe 0 (\pack -> pack ^. _ThreadPackResponse .. thread_ ^. _ThreadResponse .. id_) st.currentThread
   let mthread_post_request = st.currentThreadPost
+
   case mthread_post_request of
     Nothing                  -> pure next
     Just thread_post_request -> do
+
       epost <- rd $ postThreadPost_ByThreadId' thread_id thread_post_request
       case epost of
         Left err   -> pure next
-        Right post -> do
-          let
-            pack = defaultThreadPostPackResponse post user
-            user = userResponseToSanitizedResponse ((fromJust st.me) ^. _UserPackResponse .. user_)
-          modify (\st' -> st'{ threadPosts = M.insert (post ^. _ThreadPostResponse .. id_) pack st.threadPosts })
-          pure next
+        Right (ThreadPostResponse post) -> do
+
+          epack <- rd $ getThreadPostPack' post.id
+          case epack of
+            Left err   -> pure next
+            Right pack -> do
+              modify (\st' -> st'{ threadPosts = M.insert post.id pack st.threadPosts })
+              pure next
 
 
 
