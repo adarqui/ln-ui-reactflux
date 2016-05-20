92fce8e010e130d7f373f654069619e56f9e26f5
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-20 19:20:28 -0400

    cleanup/logging

--- src/LN/Eval/Me.purs
+++ src/LN/Eval/Me.purs
@@ -4,25 +4,30 @@ module LN.Eval.Me (
 
 
 
+import Control.Monad.Aff.Console       (log)
 import Data.Either                     (Either(..))
+import Data.Functor                    (($>))
 import Data.Maybe                      (Maybe(..))
-import Halogen                         (modify)
+import Halogen                         (modify, liftAff')
 import Optic.Core                      ((^.), (..))
-import Prelude                         (bind, pure, ($))
+import Prelude                         (bind, pure, show, ($), (<>))
 
 import LN.Api                          (rd, getMePack')
 import LN.Component.Types              (EvalEff)
 import LN.Input.Types                  (Input(..))
-import LN.T.Internal.Types
+import LN.T                            (_UserPackResponse, userId_)
 
 
 
 eval_GetMe :: EvalEff
 eval_GetMe eval (GetMe next) = do
 
-  eme <- rd $ getMePack'
-  case eme of
-    Left err -> pure next
+  e_me <- rd $ getMePack'
+
+  case e_me of
+
+    Left err -> liftAff' (log $ "eval_GetMe:" <> show err) $> next
+
     Right me -> do
-      modify (_{ me = Just me, meId = (me ^. _UserPackResponse .. user_ ^. _UserResponse .. id_) })
+      modify (_{ me = Just me, meId = (me ^. _UserPackResponse .. userId_) })
       pure next
