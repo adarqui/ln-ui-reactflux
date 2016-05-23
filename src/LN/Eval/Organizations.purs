14a6f88435d32ed037c570af6c0727e7ec9444f7
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-23 14:42:12 -0400

    new logging mechanism and cleanup

--- src/LN/Eval/Organizations.purs
+++ src/LN/Eval/Organizations.purs
@@ -9,15 +9,13 @@ module LN.Eval.Organizations (
 
 
 import Data.Either                     (Either(..))
-import Data.Functor                    (($>))
-import Data.Maybe                      (Maybe(..), maybe)
-import Halogen                         (get, gets, modify, liftAff')
-import Prelude                         (bind, pure, show, ($), (<>))
+import Data.Maybe                      (Maybe(..))
+import Halogen                         (gets, modify)
+import Prelude                         (bind, pure, ($))
 
 import LN.Api                          (rd, getOrganizationPacks')
 import LN.Api.Internal.String          as ApiS
 import LN.Component.Types              (EvalEff)
-import LN.Helpers.Log                  (log)
 import LN.Helpers.Map                  (idmapFrom)
 import LN.Input.Types                  (Input(..))
 import LN.T                            ( OrganizationPackResponses(..), OrganizationPackResponse(..)
@@ -33,7 +31,7 @@ eval_GetOrganizations eval (GetOrganizations next) = do
   e_organizations <- rd $ getOrganizationPacks'
   case e_organizations of
 
-    Left err -> liftAff' (log $ "eval_GetOrganizations: " <> show err) $> next
+    Left err                                             -> eval (AddErrorApi "eval_GetOrganizations::getOrganizationPacks'" err next)
 
     Right (OrganizationPackResponses organization_packs) -> do
 
@@ -55,7 +53,7 @@ eval_GetOrganization eval (GetOrganization org_name next) = do
   e_org <- rd $ ApiS.getOrganizationPack' org_name
   case e_org of
 
-    Left err   -> liftAff' (log $ "eval_GetOrganization: " <> show err) $> next
+    Left err   -> eval (AddErrorApi "eval_GetOrganization::ApiS.getOrganizationPack'" err next)
 
     Right pack -> do
       modify (_{ currentOrganization = Just pack })
@@ -69,7 +67,7 @@ eval_GetOrganizationForum eval (GetOrganizationForum org_name forum_name next) =
   e_forum <- rd $ ApiS.getForumPack_ByOrganizationName' forum_name org_name
   case e_forum of
 
-    Left err -> liftAff' (log $ "eval_GetOrganizationForum: " <> show err) $> next
+    Left err -> eval (AddErrorApi "eval_GetOrganizationForum::ApiS.getForumPack_ByOrganizationName'" err next)
 
     Right pack@(ForumPackResponse forum) -> do
       modify (_{ currentForum = Just pack })
@@ -81,18 +79,23 @@ eval_GetOrganizationForum eval (GetOrganizationForum org_name forum_name next) =
 eval_GetOrganizationForumBoard :: EvalEff
 eval_GetOrganizationForumBoard eval (GetOrganizationForumBoard org_name forum_name board_name next) = do
 
-  st <- get
-  let forum_id = maybe 0 (\(ForumPackResponse forum) -> forum.forumId) st.currentForum
+  m_forum <- gets _.currentForum
+  case m_forum of
+       Nothing    -> eval (AddError "eval_GetOrganizationForumBoard" "Forum doesn't exist" next)
+       Just forum -> go forum
 
-  e_board <- rd $ ApiS.getBoardPack_ByForumId' board_name forum_id
-  case e_board of
+  where
+  go (ForumPackResponse forum_pack) = do
 
-    Left err -> liftAff' (log $ "eval_GetOrganizationForumBoard: " <> show err) $> next
+    e_board <- rd $ ApiS.getBoardPack_ByForumId' board_name forum_pack.forumId
+    case e_board of
 
-    Right pack@(BoardPackResponse board) -> do
-      modify (_{ currentBoard = Just pack })
-      eval (GetThreadsForBoard board.boardId next)
-      pure next
+      Left err                             -> eval (AddErrorApi "eval_GetOrganizationForumBoard::ApiS.getBoardPack_ByForumId'" err next)
+
+      Right pack@(BoardPackResponse board) -> do
+        modify (_{ currentBoard = Just pack })
+        eval (GetThreadsForBoard board.boardId next)
+        pure next
 
 
 
@@ -101,7 +104,7 @@ eval_GetOrganizationForumBoardThread eval (GetOrganizationForumBoardThread org_n
 
   m_board <- gets _.currentBoard
   case m_board of
-    Nothing    -> liftAff' (log $ "eval_GetOrganizationForumBoardThread: No currentBoard") $> next
+    Nothing    -> eval (AddError "eval_GetOrganizationForumBoardThread" "Board doesn't exist" next)
     Just  pack -> go pack
 
   where
@@ -109,7 +112,7 @@ eval_GetOrganizationForumBoardThread eval (GetOrganizationForumBoardThread org_n
     e_thread <- rd $ ApiS.getThreadPack_ByBoardId' thread_name pack.boardId
     case e_thread of
 
-      Left err ->  liftAff' (log $ "eval_getOrganizationForumBoardThread: " <> show err) $> next
+      Left err                               -> eval (AddErrorApi "eval_getOrganizationForumBoardThread::ApiS.getThreadPack_ByBoardId'" err next)
 
       Right pack@(ThreadPackResponse thread) -> do
         modify (_{ currentThread = Just pack })
