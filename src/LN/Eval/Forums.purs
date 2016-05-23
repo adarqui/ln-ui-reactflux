77ae04fbfd9ef2ca8d3914167a5906dc6c8a14b0
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-23 14:06:24 -0400

    new logging mechanism and cleanup

--- src/LN/Eval/Forums.purs
+++ src/LN/Eval/Forums.purs
@@ -5,48 +5,33 @@ module LN.Eval.Forums (
 
 
 
-import Control.Monad.Aff.Console       (log)
-import Data.Array                      (zip)
 import Data.Either                     (Either(..))
-import Data.Functor                    (($>))
-import Data.Map                        as M
-import Halogen                         (modify, liftAff')
-import Prelude                         (bind, pure, map, show, ($), (<>))
+import Halogen                         (modify)
+import Prelude                         (bind, pure, ($))
 
 import LN.Api                          (rd, getForumPacks_ByOrganizationName')
 import LN.Component.Types              (EvalEff)
 import LN.Input.Types                  (Input(..))
+import LN.Helpers.Map                  (idmapFrom)
 import LN.T                            (ForumPackResponses(..), ForumPackResponse(..))
 
 
 
 eval_GetForums :: EvalEff
-eval_GetForums eval (GetForums next) = do
-
-  pure next
-
-{- TODO FIXME: do we use this?
-  eforums <- rd $ getForums'
-  case eforums of
-    Left err -> pure next
-    Right (ForumResponses forums) -> do
-      modify (_{ forums = forums.forumResponses })
-      pure next
-      -}
+eval_GetForums eval (GetForums next) = pure next
 
 
 
 eval_GetForumsForOrg :: EvalEff
 eval_GetForumsForOrg eval (GetForumsForOrg org_name next) = do
 
-  eforum_packs <- rd $ getForumPacks_ByOrganizationName' org_name
-  case eforum_packs of
-    Left err -> liftAff' $ log ("getForumPacks_ByOrgName: Error: " <> show err) $> next
+  e_forum_packs <- rd $ getForumPacks_ByOrganizationName' org_name
+  case e_forum_packs of
+    Left err                               -> eval (AddErrorApi "eval_GetForumsForOrg::getForumPacks_ByOrgName'" err next)
     Right (ForumPackResponses forum_packs) -> do
 
       let
-        forums     = forum_packs.forumPackResponses
-        forums_map = M.fromFoldable $ zip (map (\(ForumPackResponse pack) -> pack.forumId) forums) forums
+        forums_map = idmapFrom (\(ForumPackResponse pack) -> pack.forumId) forum_packs.forumPackResponses
 
       modify (_{ forums = forums_map })
       pure next
