0f743b8824c5ac852b103c818b1cc790b8136c7b
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-23 15:06:05 -0400

    sloppy errors view

--- src/LN/View/Errors.purs
+++ src/LN/View/Errors.purs
@@ -4,8 +4,10 @@ module LN.View.Errors (
 
 
 
+import Data.Tuple           (Tuple(..))
 import Halogen              (ComponentHTML)
 import Halogen.HTML.Indexed as H
+import Prelude              (map, ($), (<>))
 
 import LN.Input.Types       (Input)
 import LN.State.Types       (State)
@@ -15,5 +17,8 @@ import LN.State.Types       (State)
 renderView_Errors :: State -> ComponentHTML Input
 renderView_Errors st =
   H.div_ [
-    H.h1_ [ H.text "errors"]
+    H.p_ [
+      H.h1_ [ H.text "Errors"],
+      H.ul_ $ map (\(Tuple author err) -> H.li_ [H.text $ author <> " -> " <> err]) st.errors
+    ]
  ]
