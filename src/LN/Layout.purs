c55c0c8db196620f7f2a4da028807ad14fa5151a
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-24 02:51:19 -0400

    ideas

--- src/LN/Layout.purs
+++ src/LN/Layout.purs
@@ -19,31 +19,37 @@ import LN.State.Types                  (State)
 import LN.View.Module.Breadcrumbs      (renderBreadcrumbs)
 import LN.T
 
-import Data.Date as Date
-
 
 
 row :: forall a b. Array (HTML a b) -> HTML a b
 row = H.div [ P.class_ B.row ]
 
+
+
 col :: forall a b. ClassName -> Array (HTML a b) -> HTML a b
 col sz = H.div [ P.class_ sz ]
+
+
 
 col' :: forall a b. Array ClassName -> Array (HTML a b) -> HTML a b
 col' szs = H.div [ P.classes szs ]
 
+
+
 defaultLayout :: State -> Array (HTML _ _) -> HTML _ _
 defaultLayout st page =
-  H.div [ P.class_ B.containerFluid ]
-    [ header st.me (length st.errors)
-    , renderBreadcrumbs st.currentPage
-    , row page
---    , row
---        [ col' [B.colSm12] page ]
-    ]
+  H.div [ P.class_ B.containerFluid ] [
+    header st.me (length st.errors),
+    renderBreadcrumbs st.currentPage,
+    row page
+  ]
+
+
 
 -- container :: Array IProp -> HTML _ _
 container attrs = H.div (P.class_ B.container : attrs)
+
+
 
 container_ :: Array (HTML _ _) -> HTML _ _
 container_ = container []
@@ -52,34 +58,34 @@ container_ = container []
 
 header :: Maybe UserPackResponse -> Int -> HTML _ _
 header muser n_errors =
-  H.div [ P.class_ B.containerFluid ] [
-  H.nav [ P.classes [ B.navbarNav, B.navbarStaticTop] ]
-    [ container_
-      [ H.a [ P.classes [ B.navbarBrand ], linkToHref Home ]
-        [ H.text "Home" ]
-      , H.ul [ P.classes [ B.navbarNav, B.nav, B.navTabs] ]
-        [
-          H.li_ [ linkTo About "About" ]
-        , H.li_ [me]
-        , H.li_ [errors]
-        , H.li_ [ linkTo Portal "Portal" ]
-        ]
-      , case muser of
-             Nothing ->
-                 H.ul [ P.classes [ B.nav, B.navbarNav, B.navTabs, B.navbarRight ] ]
-                     [
-                        H.li_ [ linkTo Login "Log in" ]
-                     ]
-             Just u ->
-                 H.ul [ P.classes [ B.nav, B.navbarNav, B.navTabs, B.navbarRight ] ]
-                     [ H.li_ [ linkTo Logout "Log out" ] ]
-
-      ]
+  H.div [P.class_ B.containerFluid] [
+    H.nav [P.classes [B.navbarNav, B.navbarStaticTop]] [
+      container_ [
+        H.a [P.classes [B.navbarBrand], linkToHref Home] [H.text "Home"],
+        H.ul [P.classes [B.navbarNav, B.nav, B.navTabs]] [
+          H.li_ [linkTo About "About"],
+          H.li_ [me],
+          H.li_ [errors],
+          H.li_ [linkTo Portal "Portal"]
+        ],
+--        case muser of
+--          Nothing -> H.div [P.classes [B.nav, B.navbarNav, B.navTabs, B.navbarRight]] [linkTo Login "Log in"]
+--          Just u  -> H.div [P.classes [B.nav, B.navbarNav, B.navTabs, B.navbarRight]] [linkTo Logout "Log out"]
+--      ]
+        case muser of
+          Nothing ->
+            H.ul [P.classes [B.nav, B.navbarNav, B.navTabs, B.navbarRight ]] [
+              H.li_ [linkTo Login "Log in"]
+            ]
+          Just u ->
+            H.ul [P.classes [B.nav, B.navbarNav, B.navTabs, B.navbarRight]] [
+              H.li_ [linkTo Logout "Log out"] ]
+            ]
     ]
   ]
   where
   me = case muser of
-            Nothing -> linkTo NotFound "Me"
+            Nothing   -> linkTo NotFound "Me"
             Just user -> linkTo (Users (Show (user ^. _UserPackResponse .. user_ ^. _UserResponse .. nick_)) []) "Me"
   errors =
     -- TODO FIXME: use proper pill number
@@ -90,8 +96,7 @@ header muser n_errors =
 
 footer :: forall a b. HTML a b
 footer =
-  H.footer [ P.class_ (H.className "footer") ]
-    [ H.text "LN"
-    , H.ul []
-      (map (\s -> H.li [] [ H.text s ]) [ "About", "Contact" ] )
-    ]
+  H.footer [P.class_ (H.className "footer")] [
+    H.text "LN",
+    H.ul [] (map (\s -> H.li [] [H.text s]) ["About", "Contact"])
+  ]
