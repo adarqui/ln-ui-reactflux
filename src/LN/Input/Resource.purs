3eace8acfb4bb7aee06221e321fc7b34c16eb981
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-25 12:39:24 -0400

    working on adding/editing a resource

--- src/LN/Input/Resource.purs
+++ src/LN/Input/Resource.purs
@@ -4,6 +4,28 @@ module LN.Input.Resource (
 
 
 
+import Data.Maybe (Maybe)
+
+import LN.T       (ResourceType, Visibility)
+
+
+
+-- | Mod = New or Edit
+
+
 data InputResource
   = InputResource_Nop1
+  | InputResource_Mod_SetTitle String
+  | InputResource_Mod_SetDescription String
+  | InputResource_Mod_SetSource ResourceType
+  | InputResource_Mod_AddAuthor String
+  | InputResource_Mod_DelAuthor Int
+  | InputResource_Mod_EditAuthor Int String
+  | InputResource_Mod_AddCategory (Maybe (Array String))
+  | InputResource_Mod_DelCategory Int
+  | InputResource_Mod_EditCategory Int (Maybe (Array String))
+  | InputResource_Mod_SetVisibility Visibility
+  | InputResource_Mod_AddUrl String
+  | InputResource_Mod_DelUrl Int
+  | InputResource_Mod_Save (Maybe Int)
   | InputResource_Nop
