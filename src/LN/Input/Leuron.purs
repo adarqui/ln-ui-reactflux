e77a8baeec0ffd037b898aa71453b68d3a17cf03
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-27 13:51:42 -0400

    adding leurons

--- src/LN/Input/Leuron.purs
+++ src/LN/Input/Leuron.purs
@@ -1,5 +1,6 @@
 module LN.Input.Leuron (
-  InputLeuron (..)
+  InputLeuron (..),
+  Leuron_Mod (..)
 ) where
 
 
@@ -11,4 +12,51 @@ import LN.Internal.Leuron (LeuronSift)
 data InputLeuron
   = InputLeuron_Nop1
   | InputLeuron_Set_Sift LeuronSift
+  | InputLeuron_Mod
   | InputLeuron_Nop
+
+
+
+data Leuron_Mod
+  = SetTitle String
+  | EditTitle String
+  | RemTitle
+
+  | SetDescription String
+  | EditDescription String
+  | RemDescription
+
+  | SetSection String
+  | EditSection String
+  | RemSection
+
+  | SetPage Int
+  | EditPage Int
+  | RemPage
+
+  | AddExample String
+  | EditExample Int String
+  | DelExample Int
+  | ClearExamples
+
+  | AddStrength String
+  | EditStrength Int String
+  | DelStrength Int
+  | ClearStrength
+
+  | AddCategory (Array String)
+  | EditCategory Int (Array String)
+  | DelCategory Int
+  | ClearCategories
+
+  -- substitutions
+  -- splits
+
+  | AddTag String
+  | EditTag Int String
+  | DelTag Int
+  | ClearTags
+
+  | SetSpecificTo String
+  | EditSpecificTo String
+  | RemSpecificTo
