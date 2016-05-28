e602b9cc5fbea0a8e3de402674c135ba033a20bd
Andrew Darqui <andrew.darqui@gmail.com>
2016-05-27 21:53:58 -0400

    working on leuron new/edit/delete

--- src/LN/View/Resources/Leurons/Index.purs
+++ src/LN/View/Resources/Leurons/Index.purs
@@ -4,13 +4,21 @@ module LN.View.Resources.Leurons.Index (
 
 
 
-import Halogen              (ComponentHTML)
-import Halogen.HTML.Indexed as H
+import Halogen                         (ComponentHTML)
+import Halogen.HTML.Indexed            as H
+import Halogen.HTML.Properties.Indexed as P
+import Halogen.Themes.Bootstrap3       as B
 
-import LN.Input.Types       (Input)
-import LN.State.Types       (State)
+import LN.Input.Types                  (Input)
+import LN.Router.Link                  (linkToP_Glyph')
+import LN.Router.Types                 (Routes(..), CRUD(..))
+import LN.State.Types                  (State)
 
 
 
 renderView_Resources_Leurons_Index :: Int -> State -> ComponentHTML Input
-renderView_Resources_Leurons_Index resource_id _ = H.div_ [H.text "resources leurons index"]
+renderView_Resources_Leurons_Index resource_id _ =
+  H.div_ [
+    linkToP_Glyph' (ResourcesLeurons resource_id New []) B.glyphiconPlus,
+    H.p_ [H.text "resources leurons index"]
+  ]
