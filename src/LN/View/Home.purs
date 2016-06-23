db45776c10a11ea85228c82910110c1122684b52
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-23 01:47:16 -0400

    organizatons link in Home.. need to make this a main landing page though eventually

--- src/LN/View/Home.purs
+++ src/LN/View/Home.purs
@@ -4,15 +4,22 @@ module LN.View.Home (
 
 
 
-import Halogen              (ComponentHTML)
-import Halogen.HTML.Indexed as H
+import Halogen                (ComponentHTML)
+import Halogen.HTML.Indexed   as H
 
-import LN.Input.Types       (Input)
+import LN.Input.Types         (Input)
+import LN.Router.Link         (linkToP)
+import LN.Router.Class.CRUD   (CRUD(..))
+import LN.Router.Class.Routes (Routes(..))
+import LN.Router.Class.Params (emptyParams)
+
 
 
 renderView_Home :: ComponentHTML Input
 renderView_Home =
-  H.div_
-    [ H.h1_ [ H.text "Home" ]
-    , H.p_ [ H.text "home" ]
-    ]
+  H.div_ [
+   H.h1_ [ H.text "Home" ],
+   H.p_ [
+     linkToP [] (Organizations Index emptyParams) "Organizations"
+   ]
+  ]
