0efb0c5df33bb772fbdd77df7c637e5da6c08325
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-12 02:51:19 -0400

    poof

--- app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Reflex.Dom
-
-main :: IO ()
-main = mainWidget $ el "div" $ do
-  el "h1" $ text "reflex-starter"
-  el "a" $ text "Now you can start playing with Reflex!"
