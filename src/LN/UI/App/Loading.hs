c12d180831660b4933bbdfee71a7dc2a3e160248
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-27 01:37:43 -0400

    some loader helpers for values with maybes

--- src/LN/UI/App/Loading.hs
+++ src/LN/UI/App/Loading.hs
@@ -4,8 +4,11 @@
 module LN.UI.App.Loading (
   Loader (..),
   loader1,
+  loader1_,
   loader2,
+  loader2_,
   loader3,
+  loader3_,
   loadingImg
 ) where
 
@@ -33,6 +36,15 @@ loader1 loading_v loaded =
 
 
 
+loader1_ :: forall v. Loader (Maybe v) -> (v -> HTMLView_) -> HTMLView_
+loader1_ loading_v loaded =
+  case loading_v of
+    Loaded (Just v) -> loaded v
+    Loading         -> loadingImg
+    _               -> unexpectedError
+
+
+
 loader2 :: forall v1 v2. Loader v1 -> Loader v2 -> (v1 -> v2 -> HTMLView_) -> HTMLView_
 loader2 loading_v1 loading_v2 loaded =
   case (loading_v1, loading_v2) of
@@ -43,10 +55,31 @@ loader2 loading_v1 loading_v2 loaded =
 
 
 
+loader2_ :: forall v1 v2. Loader (Maybe v1) -> Loader (Maybe v2) -> (v1 -> v2 -> HTMLView_) -> HTMLView_
+loader2_ loading_v1 loading_v2 loaded =
+  case (loading_v1, loading_v2) of
+    (Loaded (Just v1), Loaded (Just v2)) -> loaded v1 v2
+    (Loading, _)                         -> loadingImg
+    (_, Loading)                         -> loadingImg
+    _                                    -> unexpectedError
+
+
+
 loader3 :: forall v1 v2 v3. Loader v1 -> Loader v2 -> Loader v3 -> (v1 -> v2 -> v3 -> HTMLView_) -> HTMLView_
 loader3 loading_v1 loading_v2 loading_v3 loaded =
   case (loading_v1, loading_v2, loading_v3) of
     (Loaded v1, Loaded v2, Loaded v3) -> loaded v1 v2 v3
+    (Loading, _, _)                   -> loadingImg
+    (_, Loading, _)                   -> loadingImg
+    (_, _, Loading)                   -> loadingImg
+    _                                 -> unexpectedError
+
+
+
+loader3_ :: forall v1 v2 v3. Loader (Maybe v1) -> Loader (Maybe v2) -> Loader (Maybe v3) -> (v1 -> v2 -> v3 -> HTMLView_) -> HTMLView_
+loader3_ loading_v1 loading_v2 loading_v3 loaded =
+  case (loading_v1, loading_v2, loading_v3) of
+    (Loaded (Just v1), Loaded (Just v2), Loaded (Just v3)) -> loaded v1 v2 v3
     (Loading, _, _)                   -> loadingImg
     (_, Loading, _)                   -> loadingImg
     (_, _, Loading)                   -> loadingImg
