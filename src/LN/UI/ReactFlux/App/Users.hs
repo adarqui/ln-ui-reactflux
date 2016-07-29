44e58658e93a7aa5ed68ed3a0fa9d5c4e5ec05b4
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-29 07:56:50 -0400

    working .. still trying to figure out how i want to do this.. need to get New working for organizations, then I can delete all of the actions/store info from App/Organizations .. when using ln-ui-core, we will be able to remove all of the per app Stores.. instead, we will just use the pure views (i think)

--- src/LN/UI/ReactFlux/App/Users.hs
+++ src/LN/UI/ReactFlux/App/Users.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Rank2Types        #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
 
@@ -10,52 +11,59 @@ module LN.UI.ReactFlux.App.Users (
   Action (..),
   store,
   view_,
-  view
+  view,
+  viewIndex,
+  viewIndex_,
+  viewIndex__
 ) where
 
 
 
-import           Control.DeepSeq                 (NFData)
-import           Control.Monad.Trans.Either      (EitherT, runEitherT)
-import           Data.Int                        (Int64)
-import           Data.Map                        (Map)
-import qualified Data.Map                        as Map
-import           Data.Rehtie                     (rehtie)
-import           Data.Typeable                   (Typeable)
-import           GHC.Generics                    (Generic)
-import           Haskell.Helpers.Either          (mustPassT)
-import           React.Flux                      hiding (view)
-import qualified React.Flux                      as RF
-import qualified Web.Bootstrap3                  as B
+import           Control.DeepSeq                      (NFData)
+import           Control.Monad.Trans.Either           (EitherT, runEitherT)
+import           Data.Int                             (Int64)
+import           Data.Map                             (Map)
+import qualified Data.Map                             as Map
+import           Data.Rehtie                          (rehtie)
+import           Data.Typeable                        (Typeable)
+import           GHC.Generics                         (Generic)
+import           Haskell.Helpers.Either               (mustPassT)
+import           React.Flux                           hiding (view)
+import qualified React.Flux                           as RF
+import qualified Web.Bootstrap3                       as B
 
-import           LN.Api                          (getUserSanitizedPacks,
-                                                  getUsersCount')
-import           LN.T.Pack.Sanitized.User        (UserSanitizedPackResponse (..), UserSanitizedPackResponses (..))
-import           LN.T.Size                       (Size (..))
-import           LN.T.User                       (UserResponse (..),
-                                                  UserSanitizedResponse (..))
-import qualified LN.UI.ReactFlux.App.Gravatar              as Gravatar
-import           LN.UI.ReactFlux.App.PageNumbers           (runPageInfo)
-import qualified LN.UI.ReactFlux.App.PageNumbers           as PageNumbers
-import           LN.UI.ReactFlux.App.Types                 (UsersMap)
+import           LN.Api                               (getUserSanitizedPacks,
+                                                       getUsersCount')
+import           LN.T.Pack.Sanitized.User             (UserSanitizedPackResponse (..), UserSanitizedPackResponses (..))
+import           LN.T.Size                            (Size (..))
+import           LN.T.User                            (UserResponse (..), UserSanitizedResponse (..))
 import           LN.UI.Core.Helpers.DataTime          (prettyUTCTimeMaybe)
 import           LN.UI.Core.Helpers.HaskellApiHelpers (rd)
 import           LN.UI.Core.Helpers.Map               (idmapFrom)
-import           LN.UI.ReactFlux.Helpers.ReactFluxDOM      (ahref, ahrefName, className_,
-                                                  classNames_)
-import           LN.UI.Core.Router                    (CRUD (..), Params, Route (..),
-                                                  RouteWith (..), routeWith')
-import           LN.UI.Core.PageInfo            (PageInfo (..),
-                                                  defaultPageInfo,
-                                                  pageInfoFromParams,
-                                                  paramsFromPageInfo)
-import           LN.UI.ReactFlux.Types                     (HTMLEvent_)
+import           LN.UI.Core.PageInfo                  (PageInfo (..),
+                                                       defaultPageInfo,
+                                                       pageInfoFromParams,
+                                                       paramsFromPageInfo)
+import           LN.UI.Core.Router                    (CRUD (..), Params,
+                                                       Route (..),
+                                                       RouteWith (..),
+                                                       routeWith')
+import           LN.UI.Core.Types
+import qualified LN.UI.ReactFlux.App.Gravatar         as Gravatar
+import           LN.UI.ReactFlux.App.Loader           (Loader (..))
+import qualified LN.UI.ReactFlux.App.Loader           as Loader
+import           LN.UI.ReactFlux.App.PageNumbers      (runPageInfo)
+import qualified LN.UI.ReactFlux.App.PageNumbers      as PageNumbers
+import           LN.UI.ReactFlux.App.Types            (UsersMap)
+import           LN.UI.ReactFlux.Helpers.ReactFluxDOM (ahref, ahrefName,
+                                                       className_, classNames_)
+import           LN.UI.ReactFlux.Types
 
 
 
 data Store = Store {
   _pageInfo :: PageInfo,
-  _l_users  :: Map Int64 UserSanitizedPackResponse
+  _l_users  :: Loader (Map Int64 UserSanitizedPackResponse)
 }
 
 
@@ -87,7 +95,7 @@ instance StoreData Store where
         pure (count, users)
       rehtie lr (const $ pure st) $ \(count, user_packs) -> do
         let new_page_info = runPageInfo count page_info
-        pure $ st{ _l_users = idmapFrom userSanitizedPackResponseUserId (userSanitizedPackResponses user_packs)
+        pure $ st{ _l_users = Loaded $ idmapFrom userSanitizedPackResponseUserId (userSanitizedPackResponses user_packs)
                  , _pageInfo = new_page_info }
 
 
@@ -100,22 +108,39 @@ store = mkStore defaultStore
 defaultStore :: Store
 defaultStore = Store {
   _pageInfo      = defaultPageInfo,
-  _l_users = Map.empty
+  _l_users = Loaded Map.empty
 }
 
 
 
-view_ :: HTMLEvent_
-view_ =
-  RF.view view () mempty
+view_ :: CRUD -> HTMLEvent_
+view_ crud =
+  RF.view view crud mempty
 
 
 
-view :: ReactView ()
-view = defineControllerView "users" store $ \Store{..} _ ->
+view :: ReactView CRUD
+view = defineControllerView "users" store $ \st@Store{..} crud ->
+  mempty
+
+
+
+viewIndex :: Store -> HTMLView_
+viewIndex Store{..} = viewIndex_ _pageInfo _l_users
+
+
+
+viewIndex_ :: PageInfo -> Loader (Map UserId UserSanitizedPackResponse) -> HTMLView_
+viewIndex_ page_info l_users = do
+  Loader.loader1 l_users (viewIndex__ page_info)
+
+
+
+viewIndex__ :: PageInfo -> Map UserId UserSanitizedPackResponse -> HTMLView_
+viewIndex__ page_info users = do
   cldiv_ B.containerFluid $ do
     h1_ "Users"
-    PageNumbers.view_ (_pageInfo, routeWith' $ Users Index)
+    PageNumbers.view_ (page_info, routeWith' $ Users Index)
     ul_ [className_ B.listUnstyled] $ do
       mapM_ (\UserSanitizedPackResponse{..} -> do
         let user = userSanitizedPackResponseUser
@@ -124,4 +149,4 @@ view = defineControllerView "users" store $ \Store{..} _ ->
             cldiv_ B.colXs1 $ p_ $ Gravatar.viewUser_ XSmall user
             cldiv_ B.colXs3 $ p_ $ ahrefName (userSanitizedResponseDisplayName user) (routeWith' $ Users (ShowS $ userSanitizedResponseName user))
             cldiv_ B.colXs2 $ p_ $ elemText $ prettyUTCTimeMaybe $ userSanitizedResponseCreatedAt user
-        ) _l_users
+        ) users
