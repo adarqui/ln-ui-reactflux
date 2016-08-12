d5185c5eec8584774ed659a312968cb02d7cf687
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-11 23:59:48 -0400

    a new view style using keys for every view function

--- src/LN/UI/ReactFlux/App/Users.hs
+++ src/LN/UI/ReactFlux/App/Users.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -12,55 +13,53 @@ module LN.UI.ReactFlux.App.Users (
 
 
 
-import           Control.DeepSeq                      (NFData)
-import           Control.Monad                        (forM_)
-import           Control.Monad.Trans.Either           (EitherT, runEitherT)
-import           Data.Int                             (Int64)
-import           Data.Map                             (Map)
-import qualified Data.Map                             as Map
-import           Data.Rehtie                          (rehtie)
-import           Data.Typeable                        (Typeable)
-import           GHC.Generics                         (Generic)
-import           Haskell.Helpers.Either               (mustPassT)
-import           React.Flux                           hiding (view)
-import qualified React.Flux                           as RF
-import qualified Web.Bootstrap3                       as B
+import           Control.DeepSeq                       (NFData)
+import           Control.Monad                         (forM_)
+import           Control.Monad.Trans.Either            (EitherT, runEitherT)
+import           Data.Int                              (Int64)
+import           Data.Map                              (Map)
+import qualified Data.Map                              as Map
+import           Data.Rehtie                           (rehtie)
+import           Data.Typeable                         (Typeable)
+import           GHC.Generics                          (Generic)
+import           Haskell.Helpers.Either                (mustPassT)
+import           React.Flux                            hiding (view)
+import qualified React.Flux                            as RF
+import qualified Web.Bootstrap3                        as B
 
-import           LN.Api                               (getUserSanitizedPacks,
-                                                       getUsersCount')
-import           LN.T.Pack.Sanitized.User             (UserSanitizedPackResponse (..), UserSanitizedPackResponses (..))
-import           LN.T.Size                            (Size (..))
-import           LN.T.User                            (UserResponse (..), UserSanitizedResponse (..))
-import           LN.UI.Core.Helpers.DataTime          (prettyUTCTimeMaybe)
-import           LN.UI.Core.Helpers.HaskellApiHelpers (rd)
-import           LN.UI.Core.Helpers.Map               (idmapFrom)
-import           LN.UI.Core.PageInfo                  (PageInfo (..),
-                                                       defaultPageInfo,
-                                                       pageInfoFromParams,
-                                                       paramsFromPageInfo)
-import           LN.UI.Core.Router                    (CRUD (..), Params,
-                                                       Route (..),
-                                                       RouteWith (..),
-                                                       routeWith')
+import           LN.Api                                (getUserSanitizedPacks,
+                                                        getUsersCount')
+import           LN.T.Pack.Sanitized.User              (UserSanitizedPackResponse (..), UserSanitizedPackResponses (..))
+import           LN.T.Size                             (Size (..))
+import           LN.T.User                             (UserResponse (..), UserSanitizedResponse (..))
+import           LN.UI.Core.Helpers.DataTime           (prettyUTCTimeMaybe)
+import           LN.UI.Core.Helpers.HaskellApiHelpers  (rd)
+import           LN.UI.Core.Helpers.Map                (idmapFrom)
+import           LN.UI.Core.PageInfo                   (PageInfo (..),
+                                                        defaultPageInfo,
+                                                        pageInfoFromParams,
+                                                        paramsFromPageInfo)
+import           LN.UI.Core.Router                     (CRUD (..), Params,
+                                                        Route (..),
+                                                        RouteWith (..),
+                                                        routeWith')
 import           LN.UI.Core.Types
-import qualified LN.UI.ReactFlux.App.Gravatar         as Gravatar
-import           LN.UI.ReactFlux.App.Loader           (Loader (..))
-import qualified LN.UI.ReactFlux.App.Loader           as Loader
-import           LN.UI.ReactFlux.App.PageNumbers      (runPageInfo)
-import qualified LN.UI.ReactFlux.App.PageNumbers      as PageNumbers
-import           LN.UI.ReactFlux.App.Types            (UsersMap)
-import           LN.UI.ReactFlux.Helpers.ReactFluxDOM (ahref, ahrefName,
-                                                       className_, classNames_)
+import qualified LN.UI.ReactFlux.App.Gravatar          as Gravatar
+import           LN.UI.ReactFlux.App.Loader            (Loader (..))
+import qualified LN.UI.ReactFlux.App.Loader            as Loader
+import           LN.UI.ReactFlux.App.PageNumbers       (runPageInfo)
+import qualified LN.UI.ReactFlux.App.PageNumbers       as PageNumbers
+import           LN.UI.ReactFlux.App.Types             (UsersMap)
+import           LN.UI.ReactFlux.Helpers.ReactFluxDOM  (ahref, ahrefName,
+                                                        className_, classNames_)
+import           LN.UI.ReactFlux.Helpers.ReactFluxView (defineViewWithSKey)
 import           LN.UI.ReactFlux.Types
 
 
 
 viewIndex :: PageInfo -> Loader (Map UserId UserSanitizedPackResponse) -> HTMLView_
-viewIndex page_info l_users = do
-  viewWithSKey go key (page_info, l_users) mempty
-  where
-  key = "users-index"
-  go  = defineView key $ \(page_info, l_users) -> do
+viewIndex !page_info !l_users = do
+  defineViewWithSKey "users-index-1" (page_info, l_users) $ \(page_info, l_users) -> do
     cldiv_ B.containerFluid $ do
       h1_ "Users"
       Loader.loader1 l_users (viewIndex_ page_info)
@@ -68,7 +67,8 @@ viewIndex page_info l_users = do
 
 
 viewIndex_ :: PageInfo -> Map UserId UserSanitizedPackResponse -> HTMLView_
-viewIndex_ page_info users = do
+viewIndex_ !page_info !users = do
+  defineViewWithSKey "users-index-2" (page_info, users) $ \(page_info, users) -> do
     PageNumbers.view_ (page_info, routeWith' $ Users Index)
     ul_ [className_ B.listUnstyled] $ do
       forM_ users $ \UserSanitizedPackResponse{..} -> do
