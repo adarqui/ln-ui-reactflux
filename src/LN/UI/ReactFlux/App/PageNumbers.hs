915a4a1c1f8ffc3009005c9399c463c7d498c156
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-17 13:59:44 -0400

    fixing page numbers

--- src/LN/UI/ReactFlux/App/PageNumbers.hs
+++ src/LN/UI/ReactFlux/App/PageNumbers.hs
@@ -20,11 +20,12 @@ import           React.Flux                            hiding (view)
 import qualified Web.Bootstrap3                        as B
 
 import           LN.UI.Core.Helpers.DataText           (tshow)
+import           LN.UI.Core.Helpers.GHCJS              (JSString)
 import           LN.UI.Core.PageInfo                   (PageInfo (..),
                                                         runPageInfo)
 import           LN.UI.Core.PageNumbers                (buildPages)
-import           LN.UI.Core.Router                     (RouteWith (..), updateParams_Offset_Limit, emptyParams)
-import LN.UI.Core.Helpers.GHCJS (JSString)
+import           LN.UI.Core.Router                     (RouteWith (..),
+                                                        emptyParams, updateParams_Offset_Limit)
 import           LN.UI.ReactFlux.Helpers.ReactFluxDOM  (ahrefName, classNames_)
 import           LN.UI.ReactFlux.Helpers.ReactFluxView (defineViewWithSKey)
 import           LN.UI.ReactFlux.Types                 (HTMLView_)
@@ -65,13 +66,13 @@ view_ !key !page_info' !route_with' =
   defineViewWithSKey key (page_info', route_with') $ \(page_info, route_with@(RouteWith route params)) -> do
     let
       (prev, pages, next, limit) = buildPages page_info route_with
-      upd off                    = RouteWith route (updateParams_Offset_Limit ((off-1)*limit) limit params)
+      route_page page            = RouteWith route (updateParams_Offset_Limit ((page-1)*limit) limit params)
     case pages of
       []     -> mempty
       _      ->
         div_ $ do
           ul_ [classNames_ [B.pagination, B.paginationSm]] $ do
-            li_ ["key" $= "pg-prev"] $ ahrefName "prev" (upd prev)
+            li_ ["key" $= "pg-prev"] $ ahrefName "prev" (route_page prev)
             forM_ (zip [(1::Int)..] pages) $
               \(idx, page_number) ->
                 if idx == 1
@@ -82,5 +83,5 @@ view_ !key !page_info' !route_with' =
                   else
                     -- else, append offset/limit to everything
                     --
-                    li_ ["key" @= idx] $ ahrefName (tshow page_number) (upd page_number)
-            li_ ["key" $= "pg-next"] $ ahrefName "next" (upd next)
+                    li_ ["key" @= idx] $ ahrefName (tshow page_number) (route_page page_number)
+            li_ ["key" $= "pg-next"] $ ahrefName "next" (route_page next)
